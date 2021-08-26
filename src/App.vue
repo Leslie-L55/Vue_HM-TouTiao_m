3f66a73acb0181bf05be2ba51818263a07ec04fe
黑马王子阿杰 <9128477+L-Hmwz_Aj@user.noreply.gitee.com>
2021-08-26 11:42:34 +0800

    下载vant引入注册并简单试用一下

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <div id="app">
     <h1 class="toutiao toutiao-dianzan2">黑马头条</h1>
     <i class="toutiao toutiao-yuedu"></i>
+    <hr>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+
+    <van-cell-group>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
     <router-view />
   </div>
 </template>
@@ -13,7 +24,7 @@ export default {
 </script>
 
 <style scoped lang="less">
-.toutiao-yuedu{
+.toutiao-yuedu {
   color: pink;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -2,7 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import './styles/index.less'
+
+import Vant from 'vant' // vant核心组件库
+import 'vant/lib/index.css' // 注册使用 vant
+
+import './styles/index.less' // vant组件的样式
+Vue.use(Vant) // 自己的全局样式
+
 Vue.config.productionTip = false
 
 new Vue({
