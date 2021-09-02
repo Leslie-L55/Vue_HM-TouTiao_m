f0036c1a145340f8d37527fddfd00ede40220b38
黑马王子阿杰 <9128477+L-Hmwz_Aj@user.noreply.gitee.com>
2021-09-03 03:48:52 +0800

    准备-创建组件并配置路由

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,14 @@
 <template>
   <div id="app">
-    <h1 class="toutiao toutiao-dianzan2">黑马头条</h1>
-    <i class="toutiao toutiao-yuedu"></i>
-    <hr />
-    <van-button type="default">默认按钮</van-button>
-    <van-button type="primary">主要按钮</van-button>
-    <van-button type="info">信息按钮</van-button>
-    <van-button type="warning">警告按钮</van-button>
-    <van-button type="danger">危险按钮</van-button>
-
-    <van-cell-group>
-      <van-cell title="单元格" value="内容" />
-      <van-cell title="单元格" value="内容" label="描述信息" />
-    </van-cell-group>
-    <div class="box">测试postcss</div>
+    <!-- 路由的出口 -->
     <router-view />
   </div>
 </template>
 
 <script>
-import { getJoke } from '@/api/news'
 export default {
-  name: 'App',
-  async created () {
-    const res = await getJoke()
-    console.log(res)
-  }
+  name: 'App'
 }
 </script>
 
-<style scoped lang="less">
-.toutiao-yuedu {
-  color: pink;
-}
-.box {
-  width: 750px;
-  height: 200px;
-  background: skyblue;
-  text-align: center;
-  line-height: 200px;
-}
-</style>
+<style scoped lang="less"></style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,14 @@ import VueRouter from 'vue-router'
 
 Vue.use(VueRouter)
 
-const routes = []
+const routes = [
+  {
+    path: '/login',
+    name: 'LoginPage',
+    // !路由的懒加载/异步加载，只有当访问 /login 这个路由地址的时候，才会加载对应组件的代码，性能高
+    component: () => import('@/views/login')
+  }
+]
 
 const router = new VueRouter({
   routes
